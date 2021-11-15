b471c823ad8965acde4c84c521272ef25102bfa0
itair <itair@jfrog.com>
2021-11-15 20:41:17 +0200

    fix GetCurrentBranchName and add GetLocalBranches function

--- utils/git.go
+++ utils/git.go
@@ -3,15 +3,17 @@ package utils
 import (
 	"fmt"
 	"github.com/go-git/go-git"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"os"
 )
 
-func getCurrentBranchName() (string, error) {
-	open, err := git.PlainOpen(".") // Get working directory, is "." enough?
+func GetCurrentBranchName() (string, error) {
+	repository, err := getLocalRepo()
 	if err != nil {
 		return "", err
 	}
 
-	head, err := open.Head()
+	head, err := repository.Head()
 	if err != nil {
 		return "", err
 	}
@@ -20,5 +22,36 @@ func getCurrentBranchName() (string, error) {
 		return "", fmt.Errorf("not branch")
 	}
 
-	return referenceName.String(), nil
+	return referenceName.Short(), nil
+}
+
+func GetLocalBranches() ([]string, error) {
+	repository, err := getLocalRepo()
+	if err != nil {
+		return nil, err
+	}
+	branches, err := repository.Branches()
+
+	res := make([]string, 0)
+	err = branches.ForEach(func(reference *plumbing.Reference) error {
+		res = append(res, reference.Name().Short())
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func getLocalRepo() (*git.Repository, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	repository, err := git.PlainOpenWithOptions(wd, &git.PlainOpenOptions{
+		DetectDotGit: true,
+	})
+	return repository, nil
 }
