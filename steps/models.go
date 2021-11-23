5216c49eeb9914607e17e965cbb87731096d76d0
itair <itair@jfrog.com>
2021-11-23 21:16:47 +0200

    Update status codes consts

--- steps/models.go
+++ steps/models.go
@@ -7,10 +7,15 @@ type Step struct {
 type StatusCode int
 
 const (
-	InProgress StatusCode = 0    // TBD
-	Wait       StatusCode = 0    // TBD
-	Success    StatusCode = 4002 // TBD
+	Queued     StatusCode = 4000
+	InProgress StatusCode = 4001
+	Success    StatusCode = 4002
 	Failure    StatusCode = 4003
 	Error      StatusCode = 4004
+	Wait       StatusCode = 4005
+	Canceled   StatusCode = 4006
+	Unstable   StatusCode = 4007
 	Skipped    StatusCode = 4008
+	TimedOut   StatusCode = 4009
+	TimingOut  StatusCode = 4014
 )
